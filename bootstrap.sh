d1d28c14742e7b97a001fe2a0463f881c0fcdaf1
Bernhard Blieninger <bblieninger@web.de>
2016-10-27 16:09:10 +0200

    First Vagrant Integration

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+
+##############################
+#
+# This is a bootstrap script which is
+# run at every startup of the vagrant machine
+# If you want to run something just once at provisioning
+# and first bootup of the vagrant machine please see
+# provision.sh
+#
+# Contributor: Bernhard Blieninger
+##############################
+
+
---
+++ provision.sh
@@ -0,0 +1,10 @@
+
+#######################
+#
+# This is a provision script
+# it will be called once when the vagrant vm is first provisioned
+# If you have commands that you want to run always please have a
+# look at the bootstrap.sh script
+#
+# Contributor: Bernhard Blieninger
+######################
