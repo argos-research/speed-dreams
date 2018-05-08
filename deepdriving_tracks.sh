a389a43760729c1d35e0eed1c399d603be965316
Alexander Weidinger <alexander.weidinger@tum.de>
2018-05-08 15:27:07 +0200

    Add shell script to download the deepdriving tracks

---
+++ deepdriving_tracks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Mirror for the track changes done by the Princeton DeepDriving project
+# 
+# http://deepdriving.cs.princeton.edu/
+curl https://nextcloud.os.in.tum.de/s/tWgLwnWN7ry92Bi/download | tar xzC data/tracks
